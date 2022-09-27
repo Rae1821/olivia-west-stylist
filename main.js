246f48d9f60171901301984d7cdb41621b1edcf0
rachel_dooley <rae1821@gmail.com>
2022-09-27 14:57:39 -0400

    Updated design of site to align with UI fundamentals

--- main.js
+++ main.js
@@ -1,35 +1,17 @@
 
 
-//Sticky Navbar
 
-//When the user scrolls the page, execute stickyFunction
-window.onscroll = function() {stickyFunction()};
+// Nav Icon
 
-//Get the header
-const header = document.getElementById("header");
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
 
-//Get the offset position of the header
-const sticky = header.offsetTop;
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('.nav-open');
+});
 
-//Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyFunction() {
-    if(window.pageYOffset >= sticky) {
-        header.classList.add("sticky")
-    } else {
-        header.classList.remove("sticky");
-    }
-}
-
-
-//Menu Icon
-
-const nav = document.querySelector(".nav");
-const container = document.querySelector(".container");
-
-function myFunction(x) {
-    x.classList.toggle("change");
-   nav.classList.toggle("show");
-   container.classList.toggle("active");
-}
-
-
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    });
+});
