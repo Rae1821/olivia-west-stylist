319b51dd9607b21fe59c3c2e71d3c049381ceefa
rachel_dooley <rae1821@gmail.com>
2022-10-13 12:18:55 -0400

    Added modal popup

--- js/main.js
+++ js/main.js
@@ -45,3 +45,49 @@ function backToTop() {
     document.documentElement.scrollTop = 0; //for Chrome, Firefox, IE, and Opera
 }
 
+
+//Modal Popup
+const modal = document.getElementById('modal');
+const modalX = document.getElementById('modal-close-btn');
+const cookieForm = document.getElementById('cookie-form');
+const modalText = document.getElementById('modal-text');
+const quizButtonTop = document.getElementById('btn__style-quiz-top');
+const quizButtonBottom = document.getElementById('btn__style-quiz-bottom');
+
+//sets a delay of 1.5 seconds before the modal pops up
+quizButtonTop.addEventListener('click', function(){
+    modal.style.display = "inline";
+})
+
+quizButtonBottom.addEventListener('click', function() {
+    modal.style.display = "inline";
+})
+
+//Closes modal when you click the X 
+modalX.addEventListener('click', function() {
+    modal.style.display = "none";
+});
+
+//Prevents reloading of form when it is closed and changes text inside HTML
+cookieForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const cookieFormData = new FormData(cookieForm);
+
+    const name = cookieFormData.get('fullName');
+
+
+    document.getElementById('modal-inner').innerHTML = `
+    <div class="modal-inner-after">
+    <h2>Thanks for signing up <span class="modal-display-name">${name}</span>!</h2>
+    <p>You are one step closer to amazing style. Your style quiz is headed to your inbox right now!</p>
+    <div style="width:100%;height:0;padding-bottom:100%;position:relative;">
+    <iframe src="https://giphy.com/embed/artj92V8o75VPL7AeQ" width="70%" height="70%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    </div>
+    </div>
+  
+    `
+  
+});
+
+
